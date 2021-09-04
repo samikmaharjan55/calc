7bda40b709e67917f583cf51aa1b494aea46a6df
Samik Maharjan <samikmaharjan55@gmail.com>
2021-09-04 22:11:42 +0545

    Main file done

--- maincalc.py
+++ maincalc.py
@@ -1,1 +1,14 @@
-# This is by Mika.
+import add_work
+import sub_work
+import mul_work
+import div_work
+
+a = 100
+b = 50
+
+total_sum = add_work.add(a,b)
+total_sub = sub_work.sub(a,b)
+total_mul = mul_work.mul(a,b)
+total_div = div_work.div(a,b)
+
+print(f"Total sum is {total_sum}. Total Sub is {total_sub}. Total Mul is {total_mul}. Total div is {total_div}")
